08bd5ea9b625176a4f5a65affbe5f9a10f772c23
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-08-04 11:00:27 -0500

    :tada: Begin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,9 +5,6 @@ const config: GatsbyConfig = {
     title: `Eliaz Bobadilla`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
@@ -19,4 +16,4 @@ const config: GatsbyConfig = {
   }]
 };
 
-export default config;
+export default confi;
