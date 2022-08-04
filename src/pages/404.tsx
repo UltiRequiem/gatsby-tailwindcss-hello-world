1c18050447ba905cc9f114ce6a4eaee954a638ad
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-08-04 13:05:53 -0500

    :sparkles: 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,30 @@ import * as React from "react";
 import { HeadFC } from "gatsby";
 
 const NotFoundPage = () => {
-  return <main>Sorry 😔, we couldn’t find what you were looking for.</main>;
+  return (
+    <section className="flex items-center h-screen p-16 dark:bg-gray-900 dark:text-gray-100">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="text-2xl font-semibold md:text-3xl">
+            Sorry, we couldn't find this page.
+          </p>
+          <p className="mt-4 mb-8 dark:text-gray-400">
+            But dont worry, you can find plenty of other things on our homepage.
+          </p>
+          <a
+            rel="noopener noreferrer"
+            href="/"
+            className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
+          >
+            Back to homepage
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export const Head: HeadFC = () => <title>Not found</title>;
